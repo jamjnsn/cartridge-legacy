c4e6bd2a8942d86c77a9dbcab66d6946f0e53eeb
Jamie Jansen <jamie@jnsn.me>
2022-05-18 16:25:25 -0700

    Create basic layout

---
+++ resources/js/components/LoadingAnimation.vue
@@ -0,0 +1,32 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="61px"
+		height="61px"
+		viewBox="0 0 100 100"
+	>
+		<circle
+			cx="50"
+			cy="50"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="15"
+			r="30"
+			stroke-dasharray="141.37166941154067 49.12388980384689"
+		>
+			<animateTransform
+				attributeName="transform"
+				type="rotate"
+				repeatCount="indefinite"
+				dur="1s"
+				values="0 50 50;360 50 50"
+				keyTimes="0;1"
+			></animateTransform>
+		</circle>
+	</svg>
+</template>
+
+<style lang="scss">
+svg {
+}
+</style>
---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+	<nav>
+		<a href="/" class="logo"><app-logo></app-logo></a>
+		<div class="search-container">
+			<input type="text" />
+		</div>
+	</nav>
+</template>
+
+<style scoped lang="scss">
+nav {
+	background: $black-light;
+	padding: 1rem;
+	display: flex;
+	flex: 0 0 auto;
+	align-items: center;
+}
+
+.logo {
+	flex: 0 0 auto;
+	display: block;
+	width: 35px;
+	height: 35px;
+	transform: scale(1);
+	transition: transform 0.1s ease;
+	margin-right: 1.5rem;
+	margin-left: 0.5rem;
+
+	&:hover {
+		transform: scale(1.1);
+		filter: drop-shadow(0 0.01em 0.4em $primary);
+	}
+}
+
+.search-container {
+	flex: 1 1 auto;
+
+	input {
+		display: block;
+		width: 100%;
+		height: 100%;
+		border: none;
+		outline: none;
+		background-color: $grey-darker;
+		border-radius: 0.4em;
+		padding: 0.5em 0.75em;
+		font-size: 1.1em;
+		color: $grey-lighter;
+		border: 0.2em solid transparent;
+
+		&:focus {
+			border-color: $primary;
+			box-shadow: 0 0.1em 0.8em transparentize($primary, 0.65);
+		}
+	}
+}
+</style>
