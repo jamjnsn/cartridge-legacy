7467a916f939ac5bf272d2c5cde6d7832d447bcd
Jamie Jansen <jamie@jnsn.me>
2022-05-18 15:28:32 -0700

    Autoload SASS variables

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,28 @@
 const mix = require('laravel-mix')
+const path = require('path')
 
 mix.browserSync('http://localhost:' + process.env.APP_PORT)
+
+mix.webpackConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve('resources/sass'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				loader: 'sass-loader',
+				options: {
+					additionalData: `
+					  @import "~@/_variables.scss";
+				`,
+				},
+			},
+		],
+	},
+})
 
 mix.copy('./static/**/*', './public')
 	.js('resources/js/app.js', 'public')
