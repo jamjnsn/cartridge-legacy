33d544fb77b1ba649ed101dbc53e8c8c5951f5c9
Jamie Jansen <jamie@jnsn.me>
2022-06-09 20:36:48 -0700

    Add Feather icons

---
+++ resources/js/components/Icon.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="icon">
+		<vue-feather class="feather" :size="size" :type="type"></vue-feather>
+	</div>
+</template>
+
+<script>
+import VueFeather from 'vue-feather'
+
+export default {
+	props: {
+		type: {
+			type: String,
+			required: true,
+		},
+		size: {
+			type: Number,
+			default: '16',
+		},
+	},
+	components: {
+		VueFeather,
+	},
+}
+</script>
+
+<style lang="scss">
+.icon {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+
+	.feather {
+		object-fit: contain;
+		object-position: center center;
+	}
+}
+</style>
